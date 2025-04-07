fa59f95c8407279b21e118732e4bdc4e1f0937e0
Ralph Nikko Predas <142536413+ralphpre24@users.noreply.github.com>
2025-04-07 08:03:27 +0800

    Cow.java

---
+++ Cow.java
@@ -0,0 +1,36 @@
+class Cow implements Animal {
+    private int legs;
+    private String sound;
+    private String food;
+    private String name;
+
+    public Cow(int legs, String sound, String food, String name) {
+        this.legs = legs;
+        this.sound = sound;
+        this.food = food;
+        this.name = name;
+    }
+
+    public Animal clone() {
+        return new Cow(this.legs, this.sound, this.food, this.name);
+    }
+
+    public void makeSound() {
+        System.out.println(this.sound);
+    }
+
+    public String getType() {
+        return "Cow";
+    }
+
+    // Getters and setters (optional)
+    public int getLegs() { return legs; }
+    public String getSound() { return sound; }
+    public String getFood() { return food; }
+    public String getName() { return name; }
+
+    public void setLegs(int legs) { this.legs = legs; }
+    public void setSound(String sound) { this.sound = sound; }
+    public void setFood(String food) { this.food = food; }
+    public void setName(String name) { this.name = name; }
+}
